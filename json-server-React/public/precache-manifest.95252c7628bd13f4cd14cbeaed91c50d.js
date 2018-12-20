99bd4827b16cd83e18e10c3c4be1d86b647212f7
Ivan Mikulyak <31531631+mikulyak98@users.noreply.github.com>
2018-12-20 09:12:28 +0200

    Add files via upload

---
+++ json-server-React/public/precache-manifest.95252c7628bd13f4cd14cbeaed91c50d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "6a1bb352829f668da0c4",
+    "url": "/static/css/main.338e8b2d.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "60ae84f22b9c25744d5b",
+    "url": "/static/js/1.60ae84f2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6a1bb352829f668da0c4",
+    "url": "/static/js/main.6a1bb352.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "60ae84f22b9c25744d5b",
+    "url": "/static/css/1.6dccaa13.chunk.css"
+  },
+  {
+    "revision": "f22396f4bf1ad8b58bff308b8301de5f",
+    "url": "/index.html"
+  }
+];
